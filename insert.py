5837a76061e4c08eba6b81b46f88e776f9ff9457
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-11-08 00:17:45 +0530

    Save as json rather than xml
    Json processing is relatively simple and fast. Hence, json is favoured. Saving
    as xml is still present but not used. Also made changes to remove redundant
    codes and use default variables.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- insert.py
+++ insert.py
@@ -4,9 +4,14 @@
 import os
 import login
 import extract
+import json
 
-def save_xml(root, notice):
-	path = root + "/" + str(notice['timestamp'])
+def save_json(path, notice):
+	f = open(path, 'w')
+	json.dump(notice, f)
+	f.close()
+
+def save_xml(path, notice):
 	f = open(path, 'w')
 	f.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 	f.write('<notice>\n')
@@ -32,7 +37,7 @@ def save_xml(root, notice):
 	f.close()
 
 
-def insert(root):
+def insert_xml(root = None):
 	if root is None:
 		root = os.path.abspath(os.path.dirname(__file__)) + "/gen/xml"
 	if not os.path.isdir(root):
@@ -42,17 +47,42 @@ def insert(root):
 		return None
 	count = 0
 	for notice in notices:
-
-		if os.path.isfile(root + "/" + str(notice['timestamp'])):
+		path = root + "/" + str(notice['timestamp']) + '.xml'
+		if os.path.isfile(path):
 			continue;
 		else:
 			count += 1
 			print "Saved notice dated '{}' titled '{}'.".format(notice['time'], notice['title'])
-			save_xml(root, notice);
+			save_xml(path, notice);
 	return count
 
+def insert_json(root = None):
+	if root is None:
+		root = os.path.abspath(os.path.dirname(__file__)) + "/gen/json"
+	if not os.path.isdir(root):
+		os.makedirs(root)
+	notices = extract.get_notice_list(False)
+	if notices is None:
+		return None
+	count = 0
+	for notice in notices:
+		path = root + '/' + str(notice['timestamp']) + '.json'
+		if os.path.isfile(path):
+			continue;
+		else:
+			count += 1
+			print "Saved notice dated '{}' titled '{}'.".format(notice['time'], notice['title'])
+			save_json(path, notice);
+	return count
+
+def insert(root = None, fltype = 'json'):
+	if fltype == 'xml':
+		return insert_xml(root)
+	else:
+		return insert_json(root)
+
 if __name__ == '__main__':
-	num = insert(None)
+	num = insert()
 	if num is not None:
 		print "Saved %d notices."%num
 
