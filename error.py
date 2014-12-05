eaa46558f9ddfd51169105ef369eb0e2c3f73f4f
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2014-12-06 00:15:33 +0530

    Custom error for TPO.
    Starting with custom error for json interactions. Should add logging capability.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ error.py
@@ -0,0 +1,16 @@
+# User defined errors for this application
+
+from time import strftime
+
+class TpoError(Exception):
+	''' Base class for exception in TPO module'''
+	pass
+
+class TpoJsonError(TpoError):
+	''' Error while JSON transaction'''
+	def __init__(self, msg):
+		self.msg = "{} : {}\n".format(strftime("%Y-%m-%d %H:%M:%S"), msg)
+
+	def __str__(self):
+		return repr(self.msg)
+
