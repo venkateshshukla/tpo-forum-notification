bb03f3ceb02ea42af768c0a28a2b101d709f7f11
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-02-22 20:10:24 +0530

    Logging for insert.py #7
    Logging integration for insert.py.
    level is set to WARNING so as not to interfere with the script output.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- insert.py
+++ insert.py
@@ -2,17 +2,26 @@
 # Script to get the list of notices and save them locally
 
 import os
+import logging
+
 import extract
 from notice import Notice
 
 # Save the notices as json in gen/json folder by parsing gen/notice_board.html
 def insert(root = None):
+	logging.debug("called : insert")
+	logging.debug("argument root : %s", root)
+
 	if root is None:
+		logging.debug("empty root is received")
 		root = os.path.abspath(os.path.dirname(__file__)) + "/gen/json"
 	if not os.path.isdir(root):
+		logging.debug("making directory : %s", root)
 		os.makedirs(root)
+
 	notices = extract.get_notice_list(False)
 	if notices is None:
+		logging.error("error getting notice list")
 		return None
 	count = 0
 	for notice in notices:
@@ -25,16 +34,25 @@ def insert(root = None):
 		else:
 			count += 1
 			print "Saved notice dated '{}' titled '{}'.".format(notice['time'], notice['title'])
+			logging.info("Saved notice dated %s titled %s",
+					notice['time'], notice['title'])
+
 			n = Notice(timestamp)
 			n.save_json(notice)
+
+	logging.info("%d notices inserted", count)
 	return count
 
 # If run as standalone script, call insert()
 if __name__ == '__main__':
+	log_level = logging.WARNING
+	log_format = "%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s()\t%(message)s"
+	logging.basicConfig(format=log_format, level=log_level)
+
+	logging.info("starting %s", __file__)
 	num = insert()
-	from time import strftime
-	print strftime("%Y-%m-%d %H:%M:%S")
-	print __file__
 	if num is not None:
+		logging.info("saved %d notices", num)
 		print "Saved %d notices."%num
+	logging.info("finished %s", __file__)
 
