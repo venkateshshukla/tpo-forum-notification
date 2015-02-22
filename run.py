894d5c4fdafbcb763cffd456ab6866602b4997aa
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-02-22 14:46:31 +0530

    Beginning logging of application #7
    As my experience with various bugs suggest, it is extremely important to have
    logging in your application. For this, I have started integrating python logging
    module with this application.
    
    This commit represents the initial stab at logging integration.
    Logs are sent to /logs/date.log file and are of DEBUG level giving us a huge
    amount of logs to study.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- run.py
+++ run.py
@@ -2,25 +2,42 @@
 # The python script that is run on every minute and does all the task
 # sequentially
 
+import os
+import time
+import logging
+
 import insert
 import update
 import send
 from login import TpoSession
 
+log_dir = os.path.abspath(os.path.dirname(__file__)) + '/logs'
+if not os.path.isdir(log_dir):
+	logging.debug("making directory : %s", log_dir)
+	os.makedirs(log_dir)
+log_level = logging.DEBUG
+log_file = "{}/{}.log".format(log_dir, time.strftime("%Y_%m_%d"))
+log_format = "%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s\t%(message)s"
+logging.basicConfig(filename=log_file, format=log_format, level=log_level)
+
 # Login and save the html file to gen
+logging.info('Login and saving notice board html to gen')
 tpo = TpoSession()
 tpo.login()
 
 # From the html file, extract and save the notices
+logging.info('Extracting notice from notice board html.')
 num = insert.insert()
 if num is None:
-	print "Error encountered. Exiting."
+	logging.error("Error encountered during extraction. Exiting.")
 	exit()
-print "Inserted %d notices."%num
+logging.info("Inserted %d notices.", num)
 
 # Update the json files to include the notice details and attachments
+logging.info('Updating json files')
 update.update()
 
 # Send the unsent notices
+logging.info('Sending unsent notices.')
 send.send_unsent()
 
