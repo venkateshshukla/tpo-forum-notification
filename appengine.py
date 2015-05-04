e30723a97659c5ce6b2425d72e76e17541e4a058
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-05-04 18:00:06 +0530

    Slight modifications in order to run in GAE
    Google App Engine assigns python methods to urls. So, run_sequence() method must
    be run on any GET request on /run url. Hence, run.py has been modified to make
    it runnable from another script. appengine.py does exactly this. The url -
    application bindings has been set in app.yaml.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ appengine.py
@@ -0,0 +1,9 @@
+import webapp2
+import run
+
+class AppEngine(webapp2.RequestHandler):
+    def get(self):
+        run.run_sequence()
+        self.response.write('Executed.')
+
+app = webapp2.WSGIApplication([ ('/run', AppEngine), ], debug=True)
