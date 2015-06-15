53b49a79633a77aca36a3b7fde36bd59fd0d2416
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-15 09:20:44 +0530

    As run would be run by scheduler, make it a method
    Also, logging level changed to info for fewer logs.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

--- run.py
+++ run.py
@@ -11,33 +11,38 @@ import update
 import send
 from login import TpoSession
 
-log_dir = os.path.abspath(os.path.dirname(__file__)) + '/logs'
-if not os.path.isdir(log_dir):
-	logging.debug("making directory : %s", log_dir)
-	os.makedirs(log_dir)
-log_level = logging.DEBUG
-log_file = "{}/{}.log".format(log_dir, time.strftime("%Y_%m_%d"))
-log_format = "%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s()\t%(message)s"
-logging.basicConfig(filename=log_file, format=log_format, level=log_level)
+def run():
+	# Login and save the html file to gen
+	logging.info('Login and saving notice board html to gen')
+	tpo = TpoSession()
+	tpo.login()
 
-# Login and save the html file to gen
-logging.info('Login and saving notice board html to gen')
-tpo = TpoSession()
-tpo.login()
+	# From the html file, extract and save the notices
+	logging.info('Extracting notice from notice board html.')
+	num = insert.insert()
+	if num is None:
+		logging.error("Error encountered during extraction. Exiting.")
+		exit()
+	logging.info("Inserted %d notices.", num)
 
-# From the html file, extract and save the notices
-logging.info('Extracting notice from notice board html.')
-num = insert.insert()
-if num is None:
-	logging.error("Error encountered during extraction. Exiting.")
-	exit()
-logging.info("Inserted %d notices.", num)
+	# Update the json files to include the notice details and attachments
+	logging.info('Updating json files')
+	update.update()
 
-# Update the json files to include the notice details and attachments
-logging.info('Updating json files')
-update.update()
+	# Send the unsent notices
+	logging.info('Sending unsent notices.')
+	send.send_unsent()
 
-# Send the unsent notices
-logging.info('Sending unsent notices.')
-send.send_unsent()
+	logging.info("Finished running script.")
 
+if __name__ == '__main__':
+	log_dir = os.path.abspath(os.path.dirname(__file__)) + '/logs'
+	if not os.path.isdir(log_dir):
+		logging.debug("making directory : %s", log_dir)
+		os.makedirs(log_dir)
+
+	log_level = logging.INFO
+	log_file = "{}/{}.log".format(log_dir, time.strftime("%Y_%m_%d"))
+	log_format = "%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s()\t%(message)s"
+	logging.basicConfig(filename=log_file, format=log_format, level=log_level)
+	run()
