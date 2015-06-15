435d682cf26938a0d12fe6471686fe48fe733481
Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>
2015-06-15 09:22:22 +0530

    Use apscheduler to call `run` script every 3 min
    Using a blocking scheduler, call run script every 3 min. The run script does
    everything - extract the notices, updates them and sends the information to
    subscribers.
    
    Signed-off-by: Venkatesh Shukla <venkatesh.shukla.eee11@iitbhu.ac.in>

---
+++ schedule.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# Schedule tasks to run at specific intervals without using crontab per se.
+# This is useful for platform independent implementation and for deploying to
+# interfaces such as heroku.
+
+import logging
+import os
+import time
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from run import run
+
+def run_job():
+	log_dir = os.path.abspath(os.path.dirname(__file__)) + '/logs'
+	if not os.path.isdir(log_dir):
+		logging.debug("making directory : %s", log_dir)
+		os.makedirs(log_dir)
+
+	log_level = logging.INFO
+	log_file = "{}/{}.log".format(log_dir, time.strftime("%Y_%m_%d"))
+	log_format = "%(asctime)s\t%(levelname)s\t%(filename)s\t%(funcName)s()\t%(message)s"
+	logging.basicConfig(filename=log_file, format=log_format, level=log_level)
+	run()
+
+if __name__ == '__main__':
+	sched = BlockingScheduler()
+	sched.add_job(run_job, 'interval', minutes=3)
+	try:
+	        sched.start()
+	except (KeyboardInterrupt, SystemExit):
+	        pass
